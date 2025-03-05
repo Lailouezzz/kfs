5d51a809c10ae5d5047c51a35aae9c5e16a8ae70
Antoine Massias <massias.antoine.pro@gmail.com>
2025-03-05 14:38:42 +0100

    Early version of printk.
    Added: Very rudimentary version of printk.

--- kernel/arch/i386/kernel/main.c
+++ kernel/arch/i386/kernel/main.c
@@ -1,6 +1,7 @@
 #include <multiboot.h>
 
 #include <kfs/vga.h>
+#include <kfs/kernel.h>
 
 extern void	init_gdt(void);
 
@@ -19,8 +20,7 @@ void	arch_main(struct multiboot_info *mb_info, unsigned int magic)
 	}
 	init_gdt();
 	vga_setup();
-	vga_print_string("42");
-	vga_print_char('\n');
+	vga_print_string("42\n");
 	vga_print_string_c("Colored kernel baby hehe", 0x04);
 	vga_move_cursor(7, 7);
 	vga_print_string_c("Movable cursor too !!", 0x5);
---
+++ kernel/kernel/printk.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printk.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Antoine Massias <massias.antoine.pro@gm    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/05 12:22:18 by Antoine Mas       #+#    #+#             */
+/*   Updated: 2025/03/05 14:31:44 by Antoine Mas      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <kfs/kernel.h>
+#include <kfs/vga.h>
+#include <stdarg.h>
+
+static
+int	_print_int(
+		int n
+		);
+
+static
+int	_print_hex(
+		unsigned int n
+		);
+
+int	printk(
+		const char *fmt,
+		...
+		)
+{
+	int	n = 0;
+
+	// d x s c
+
+	va_list args;
+	va_start(args, fmt);
+
+	while (*fmt)
+	{
+		if (*fmt++ != '%')
+		{
+			vga_print_char(fmt[-1]);
+			++n;
+			continue ;
+		}
+		if (*fmt == '\0')
+			break ;
+		switch (*fmt++)
+		{
+			case '%':
+				n += vga_print_char('%');
+				break ;
+			case 'c':
+				n += vga_print_char((char)va_arg(args, int));
+				break ;
+			case 'd':
+				n += _print_int(va_arg(args, int));
+				break ;
+			case 's':
+				n += vga_print_string(va_arg(args, char *));
+				break ;
+			case 'x':
+				n += _print_hex(va_arg(args, unsigned int));
+				break ;
+			default:
+				break ;
+		}
+	}
+	va_end(args);
+	return (n);
+}
+
+static
+int	__print_int(unsigned int n)
+{
+	if (n > 9)
+		return (_print_int(n / 10) + vga_print_char('0' + (n % 10)));
+	return (vga_print_char('0' + (n % 10)));
+}
+
+static
+int	_print_int(
+		int n
+		)
+{
+	int r = 0;
+	unsigned int _n = *(unsigned int *)&n;
+
+	if (n < 0)
+	{
+		r += vga_print_char('-');
+		_n = 1 << 31;
+	}
+	return (__print_int(_n));
+}
+
+static
+int	_print_hex(
+		unsigned int n
+		)
+{
+	if (n > 9)
+		return (_print_int(n / 16) + vga_print_char('0' + (n % 16)));
+	return (vga_print_char('0' + (n % 16)));
+}
