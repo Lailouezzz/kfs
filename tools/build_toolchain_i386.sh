6b007eaaa649950e65a6bfd84147e8a7a5d087ee
Antoine Massias <massias.antoine.pro@gmail.com>
2025-04-07 21:23:01 +0200

    Improved toolchain creation and env management.
    - Added: env file to source.
    - Changed: The toolchain download & build script
    	(/tools/build_toolchain_i386.sh) is now more
    	modular and it builds the toolchain into the
    	KFS's root directory instead of
    	`$HOME/opt/cross`.

--- tools/build_toolchain_i386.sh
+++ tools/build_toolchain_i386.sh
@@ -1,38 +1,89 @@
-#! /bin/sh
+#!/bin/sh
 
-export PREFIX="$HOME/opt/cross"
+# This script *MUST* be run it's directory and *NOT*
+# from the root of the project !
+
+BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/binutils-2.44.tar.gz
+GCC_URL=https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.gz
+GDB_URL=https://ftp.gnu.org/gnu/gdb/gdb-16.2.tar.gz
+
+BINUTILS_ARCHIVE_NAME=$(basename $BINUTILS_URL .tar.gz)
+GCC_ARCHIVE_NAME=$(basename $GCC_URL .tar.gz)
+GDB_ARCHIVE_NAME=$(basename $GDB_URL .tar.gz)
+
+PWD=$(pwd)
+export PREFIX="$PWD/cross"
 export TARGET=i386-elf
 export PATH="$PREFIX/bin:$PATH"
 
-mkdir build-toolchain && cd build-toolchain
+NPROC=$(nproc)
+ROOT="$PWD"/build-toolchain
 
-wget https://ftp.gnu.org/gnu/gdb/gdb-16.2.tar.gz
-wget https://ftp.gnu.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.gz
-wget https://ftp.gnu.org/gnu/binutils/binutils-2.44.tar.gz
+mkdir -p "$PREFIX" "$ROOT"
+cd "$ROOT"
 
-tar xvf gdb-16.2.tar.gz
-tar xvf gcc-14.2.0.tar.gz
-tar xvf binutils-2.44.tar.gz
+if ! (wget --continue "$BINUTILS_URL" && tar xf "$BINUTILS_ARCHIVE_NAME".tar.gz)
+then
+	echo failed to download or extract BINUTILS >&2
+	exit 1
+fi
 
-mkdir build-binutils
-mkdir build-gcc
-mkdir build-gdb
+if ! (wget --continue "$GCC_URL" && tar xf "$GCC_ARCHIVE_NAME".tar.gz)
+then
+	echo failed to download or extract GCC >&2
+	exit 1
+fi
 
-cd build-binutils
-../binutils-2.44/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-make -j$(nproc)
+if ! (wget --continue "$GDB_URL" && tar xf "$GDB_ARCHIVE_NAME".tar.gz)
+then
+	echo failed to download or extract GDB >&2
+	exit 1
+fi
+
+cd "$ROOT/$BINUTILS_ARCHIVE_NAME"
+mkdir build
+cd build
+../configure				\
+	--target=$TARGET		\
+	--prefix="$PREFIX"		\
+	--with-sysroot			\
+	--disable-nls			\
+	--disable-werror
+make -j$NPROC
 make install
 
-cd ../build-gdb
-../gdb-16.2/configure --target=$TARGET --prefix="$PREFIX" --disable-werror
-make all-gdb -j$(nproc)
-make install-gdb
+cd "$ROOT"
+rm -rf "$BINUTILS_ARCHIVE_NAME"
 
-cd ../build-gcc
-../gcc-14.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --disable-hosted-libstdcxx
-make all-gcc -j$(nproc)
-make all-target-libgcc -j$(nproc)
-make all-target-libstdc++-v3 -j$(nproc)
+cd "$ROOT/$GCC_ARCHIVE_NAME"
+mkdir build
+cd build
+../configure					\
+	--target=$TARGET			\
+	--prefix="$PREFIX"			\
+	--disable-nls				\
+	--enable-languages=c,c++	\
+	--without-headers			\
+	--disable-hosted-libstdcxx
+make all-gcc -j$NPROC
+make all-target-libgcc -j$NPROC
+make all-target-libstdc++-v3 -j$NPROC
 make install-gcc
 make install-target-libgcc
 make install-target-libstdc++-v3
+
+cd "$ROOT"
+rm -rf "$GCC_ARCHIVE_NAME"
+
+cd "$ROOT/$GDB_ARCHIVE_NAME"
+mkdir build
+cd build
+../configure			\
+	--target=$TARGET	\
+	--prefix="$PREFIX"	\
+	--disable-werror
+make all-gdb -j$NPROC
+make install-gdb
+
+cd "$ROOT"
+rm -rf "$GDB_ARCHIVE_NAME"
